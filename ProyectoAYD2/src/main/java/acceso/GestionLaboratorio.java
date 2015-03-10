8e3cb2dc85706001937d9c0b3ceff4b7b58c9dd0
braestuart <braynernavarro@gmail.com>
2015-03-10 12:34:58 -0600

    Creacion de insercion curso, salon

--- ProyectoAYD2/src/main/java/acceso/GestionLaboratorio.java
+++ ProyectoAYD2/src/main/java/acceso/GestionLaboratorio.java
@@ -5,10 +5,59 @@
  */
 package acceso;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 /**
  *
  * @author bray
  */
 public class GestionLaboratorio {
-    
+    private Conexion nuevaConexion;
+    private Connection conn;
+    public GestionLaboratorio()
+    {
+        nuevaConexion= new Conexion();
+         conn=nuevaConexion.getConn();
+    }
+    public void insertarCurso(String name)
+    {
+        
+        nuevaConexion.crearConn();
+        conn=nuevaConexion.getConn();
+         Statement s = null;
+        try
+        {
+            s = conn.createStatement();
+            //seleccionamos la tabla de la base de datos la cual lleva por nombre trabajadores
+            //System.out.println("INSERT INTO curso values(default,'"+name+"');");
+            s.executeUpdate("INSERT INTO curso values(default,'"+name+"');");
+            
+        }catch (Exception e)
+        {
+            System.out.println("Problema al insertar en curso. "+e);
+        }
+        nuevaConexion.cierraConn();
+
+    }
+    public void insertarSalon(String name,int capacidad)
+    {
+        
+        nuevaConexion.crearConn();
+        conn=nuevaConexion.getConn();
+         Statement s = null;
+        try
+        {
+            s = conn.createStatement();
+            //seleccionamos la tabla de la base de datos la cual lleva por nombre trabajadores
+            //System.out.println("INSERT INTO curso values(default,'"+name+"');");
+            s.executeUpdate("INSERT INTO salon values(default,'"+name+"',"+capacidad+");");
+            
+        }catch (Exception e)
+        {
+            System.out.println("Problema al insertar en salon. "+e);
+        }
+        nuevaConexion.cierraConn();
+
+    }    
 }
--- ProyectoAYD2/src/main/java/acceso/GestionPrograma.java
+++ ProyectoAYD2/src/main/java/acceso/GestionPrograma.java
@@ -5,64 +5,13 @@
  */
 package acceso;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
  * @author bray
  */
 public class GestionPrograma {
-    private Connection conn;
-    public GestionPrograma() 
-    {
-        
-    }
     
-    public void crearConn()
-    {
-        String driver = "org.postgresql.Driver"; // el nombre de nuestro driver Postgres.
-        String connectString = "jdbc:postgresql://localhost:5433/ProyectoAYD2"; // llamamos nuestra bd
-        String user = "postgres"; // usuario postgres
-        String password = "root"; // no tiene password nuestra bd.
-        try {
-            Class.forName(driver);//Hacemos la coneccion.
-            conn = DriverManager.getConnection(connectString, user, password);//Si la Conexion fue realizada con exito, muestra el sgte mensaje.
-            //devolver="Conexion a la base de datos Ejemplo realizada con exito! ";
-            //Cerramos la Conexion
-            //conn.close();
-        }
-//Si se produce una Excepcion y no nos podemos conectar, muestra el sgte. mensaje.
-catch(SQLException e) {
-    System.out.println("Se ha producido un error en la conexion a la base de datos Ejemplo! "+e);
-}       catch (ClassNotFoundException ex) {
-            Logger.getLogger(GestionPrograma.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("fallo la conexion "+ex.toString());
-        }
-        
-        //return devolver;
-        
-        //return devolver;
-    }
-
-    public Connection getConn() {
-        return conn;
-    }
-            
-       public void cierraConn()
-    {
-        try
-        {
-            conn.close();
-        }catch(Exception e)
-        {
-            System.out.println("Problema para cerrar la Conexión a la base de datos" );
-        }
-    }
     
 }
