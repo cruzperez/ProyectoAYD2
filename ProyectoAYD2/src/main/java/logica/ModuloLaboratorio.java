6e880c3bdc005edd02e5f207cae42634c67aa8f1
BryanValenzuela <bevm17@gmail.com>
2015-03-11 14:38:28 -0600

    Agregar metodo de obtenerCursos y obtenerCursosNombre

--- ProyectoAYD2/src/main/java/logica/ModuloLaboratorio.java
+++ ProyectoAYD2/src/main/java/logica/ModuloLaboratorio.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 public class ModuloLaboratorio {
     
     private GestionLaboratorio lab = new GestionLaboratorio();
+    private int Hola;
     
     public boolean modificarSalon(int id, String salon, int capacidad){
         try{
