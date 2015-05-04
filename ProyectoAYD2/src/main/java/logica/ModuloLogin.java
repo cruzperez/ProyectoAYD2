e115a5f7aaf2d94b1a3acb2c6d9fc2c8ad80c442
VIKTORCORADOV <viktorcoradov@outlook.com>
2015-05-04 14:59:46 -0600

    login 0.0.2

--- ProyectoAYD2/src/main/java/logica/ModuloLogin.java
+++ ProyectoAYD2/src/main/java/logica/ModuloLogin.java
@@ -15,15 +15,25 @@ public class ModuloLogin {
     private GestionLogin prog = new GestionLogin();
 
     public boolean obtenerCredenciales(String usuario, String contrasena) {
+        String nombre = usuario;
+        String correo = usuario;
+        String pass = contrasena;
+        String nombre1 = "";
+        String correo1 = "";
+        String pass1 = "";
         try {
             String[] programas = prog.retornarUsuario().split("/");
             for (int i = 0; i < programas.length; i++) {
-                if (programas[i].split(",")[0].equals(usuario) || programas[i].split(",")[1].equals(usuario)
-                        && programas[i].split(",")[0].equals(contrasena)) {
-                    return true;
+                nombre1 = programas[i].split(",")[0];
+                correo1 = programas[i].split(",")[1];
+                pass1 = programas[i].split(",")[2];
+                if (nombre.equals(nombre1) || correo.equals(correo1)) {
+                    if (pass1.equals(pass1)) {
+                        return true;
+                    }
                 }
             }
-            return true;
+            return false;
         } catch (Exception e) {
             return false;
         }
